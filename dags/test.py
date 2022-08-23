c35bc98ecad0a7369565b4cbc92320b1e9432412
Adam Arafa Røe <adamarafaroe@nav.no>
2022-08-23 15:09:04 +0200

    lagt til dummy dag

---
+++ dags/test.py
@@ -0,0 +1,17 @@
+from airflow.models import DAG, Variable
+from airflow.utils.dates import datetime
+from dataverk_airflow.knada_operators import create_knada_python_pod_operator
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id = 'test_dag', 
+    start_date = datetime(2022, 9, 1), # start date for the dag
+    schedule_interval = None, #'@monthly' , #timedelta(days=1), schedule_interval='*/5 * * * *',
+) as dag:
+
+    test_task = EmptyOperator(
+        task_id = 'test'
+    )
+
+    
+test_task
