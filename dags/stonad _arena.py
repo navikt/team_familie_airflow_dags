3871f4d7798381742676239f91166b791fdeadb3
Adam Arafa Røe <adamarafaroe@nav.no>
2022-08-23 16:35:57 +0200

    oppdatert start_date til 01.08

--- dags/stonad _arena.py
+++ dags/stonad _arena.py
@@ -4,7 +4,7 @@ from dataverk_airflow.knada_operators import create_knada_python_pod_operator
 
 with DAG(
     dag_id = 'dbt_dag', 
-    start_date = datetime(2022, 9, 1), # start date for the dag
+    start_date = datetime(2022, 8, 1), # start date for the dag
     description = 'An Airflow DAG to invoke dbt stonad_arena project and a Python script to insert into fam_ef_stonad_arena ',
     schedule_interval = None, #'@monthly' , #timedelta(days=1), schedule_interval='*/5 * * * *',
     catchup = False # makes only the latest non-triggered dag runs by airflow (avoid having all dags between start_date and current date running)
