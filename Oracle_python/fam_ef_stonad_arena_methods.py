82276d4c44a53556e9e157c6d262896be3568675
Adam Arafa Røe <adamarafaroe@nav.no>
2022-08-31 15:42:31 +0200

    cleaned up

--- Oracle_python/fam_ef_stonad_arena_methods.py
+++ Oracle_python/fam_ef_stonad_arena_methods.py
@@ -23,15 +23,6 @@ def send_context(conn, cur):
     cur.execute(sql)
     conn.commit()
 
-# def delete_data(periode):
-#     """
-#     sletter data fra fam_ef_stonad_arena med periode som kriteriea.
-#     :param periode:
-#     :return:
-#     """
-#     sql = ('delete from dvh_fam_ef.fam_ef_stonad_arena where periode =: periode')
-#     connection(sql)
-
 def delete_data(conn, cur, periode):
     """
     sletter data fra fam_ef_stonad_arena med periode som kriteriea.
