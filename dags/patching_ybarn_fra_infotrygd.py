107b232e3a3c81a205e10c9215c10bf5cd540d76
Adam Arafa Røe <adamarafaroe@nav.no>
2022-10-31 09:50:00 +0100

    schedulert patching av ybarn

--- dags/patching_ybarn_fra_infotrygd.py
+++ dags/patching_ybarn_fra_infotrygd.py
@@ -51,3 +51,5 @@ with DAG(
         python_callable=fam_ef_patch_ybarn.patch_ybarn_arena,
         op_kwargs = {**op_kwargs, 'periode':periode}
     )
+
+start_alert >> send_context_information >> patch_ybarn_arena
