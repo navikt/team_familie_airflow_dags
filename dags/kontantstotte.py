eca93e8ac9e59eb5e5b56e435057ddea9a3e1aeb
Adam Arafa Røe <adamarafaroe@nav.no>
2022-11-17 14:47:59 +0100

    gjøre endringer for å kjøre en test applikasjon i dev

--- dags/kontantstotte.py
+++ dags/kontantstotte.py
@@ -10,14 +10,16 @@ import pendulum
 
 with DAG(
   dag_id="fam_ef_consumer_test",
-  start_date=datetime(2022, 8, 9),
-  schedule_interval="@hourly",
+  start_date=datetime(2022, 11, 15),
+  schedule_interval = None, #"@hourly",
   max_active_runs=1
 ) as dag:
 
   consumer = kafka_consumer_kubernetes_pod_operator(
     task_id = "ef-kafka-consumer_test",
     config = ks.config,
+    data_interval_start_timestamp_milli=1634688000000, # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
+    data_interval_end_timestamp_milli=1668470400000,
     slack_channel = Variable.get("slack_error_channel")
   )
 
