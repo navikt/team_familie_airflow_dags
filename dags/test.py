edf2b64bedef9b38d1f6f00e6f8607165a42b57a
Adam Arafa Røe <adamarafaroe@nav.no>
2022-11-30 15:38:52 +0100

    endret test dag

--- dags/test.py
+++ dags/test.py
@@ -3,6 +3,8 @@ from airflow.utils.dates import datetime
 from dataverk_airflow.knada_operators import create_knada_python_pod_operator
 from airflow.operators.dummy_operator import DummyOperator
 from operators.slack_operator import slack_info
+from airflow.decorators import task
+
 
 with DAG(
     dag_id = 'test_dag', 
@@ -10,11 +12,11 @@ with DAG(
     schedule_interval = None, #'@monthly' , #timedelta(days=1), schedule_interval='*/5 * * * *',
 ) as dag:
 
-    test_task = slack_info(
-        task_id = 'test',
-        message = "en test"
-
-    )
+    @task
+    def slack():
+        slack_info(
+            message = "en test"
+        )
 
  
-test_task
+    test = slack()
