f281786f113bb8b780406fead9d5380d160c5fa7
Adam Arafa Røe <adamarafaroe@nav.no>
2022-12-15 09:47:09 +0100

    rekjorer en periode

--- dags/kontantstotte_kafka.py
+++ dags/kontantstotte_kafka.py
@@ -7,15 +7,15 @@ from operators.kafka_operators import kafka_consumer_kubernetes_pod_operator
 with DAG(
   dag_id="kontantstotte_read_kafka_topic",
   start_date=datetime(2022, 11, 27),
-  schedule_interval= "@hourly",
+  schedule_interval= None,"@hourly",
   max_active_runs=1
 ) as dag:
 
   consumer = kafka_consumer_kubernetes_pod_operator(
     task_id = "kontantstotte_hent_kafka_data",
     config = ks.config,
-    #data_interval_start_timestamp_milli="1668470400000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
-    #data_interval_end_timestamp_milli="1671005338000",   # from first day we got data until 15.11.2022 (todays before todays date)
+    data_interval_start_timestamp_milli="1668470400000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
+    data_interval_end_timestamp_milli="1671005338000",   # from first day we got data until 15.11.2022 (todays before todays date)
     slack_channel = Variable.get("slack_error_channel")
   )
 
