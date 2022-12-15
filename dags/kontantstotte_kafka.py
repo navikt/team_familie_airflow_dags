64b44d5d5212d10f2e766d386dc2d7648920bb49
Adam Arafa Røe <adamarafaroe@nav.no>
2022-12-15 09:35:50 +0100

    endret start timestamp

--- dags/kontantstotte_kafka.py
+++ dags/kontantstotte_kafka.py
@@ -14,7 +14,7 @@ with DAG(
   consumer = kafka_consumer_kubernetes_pod_operator(
     task_id = "kontantstotte_hent_kafka_data",
     config = ks.config,
-    data_interval_start_timestamp_milli="1669593600000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
+    data_interval_start_timestamp_milli="1668470400000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
     data_interval_end_timestamp_milli="1671005338000",   # from first day we got data until 15.11.2022 (todays before todays date)
     slack_channel = Variable.get("slack_error_channel")
   )
