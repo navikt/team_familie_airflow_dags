e0aeb286b469fc8b16406a1cd079b95784c7e56d
Adam Arafa Røe <adamarafaroe@nav.no>
2022-12-20 15:30:26 +0100

    litt oppusing i filene

--- Oracle_python/fam_ef_stonad_arena.py
+++ Oracle_python/fam_ef_stonad_arena.py
@@ -1,37 +1,6 @@
 from felles_metoder import get_periode
 from utils.db.oracle_conn import oracle_conn
 
-# def delete_data(conn, cur, periode):
-#     """
-#     sletter data fra fam_ef_stonad_arena med periode som kriteriea.
-#     :param periode:
-#     :return:
-#     """
-#     sql = (f"delete from dvh_fam_ef.fam_ef_stonad_arena where periode = {periode}")
-#     cur.execute(sql)
-#     conn.commit()
-
-# def insert_data(conn, cur):
-#     """
-#     insert data fra ef_stonad_arena_final (view laget med dbt) into fam_ef_stonad_arena
-#     :param:
-#     :return:
-#     """
-#     sql = ('''
-#             INSERT INTO dvh_fam_ef.fam_ef_stonad_arena (FK_PERSON1,FK_DIM_PERSON,PERIODE,ALDER,KOMMUNE_NR,BYDEL_NR,KJONN_KODE,MAALGRUPPE_KODE
-#             ,MAALGRUPPE_NAVN,STATSBORGERSKAP,FODELAND,SIVILSTATUS_KODE,ANTBLAV,ANTBHOY,BARN_UNDER_18_ANTALL,INNTEKT_SISTE_BERAAR,INNTEKT_3_SISTE_BERAAR
-#             ,UTDSTONAD,TSOTILBARN,TSOLMIDLER,TSOBOUTG,TSODAGREIS,TSOREISOBL,TSOFLYTT,TSOREISAKT,TSOREISARB,TSOTILFAM,YBARN
-#             ,ANTBARN,ANTBU1,ANTBU3,ANTBU8,ANTBU10,ANTBU18,KILDESYSTEM,LASTET_DATO,OPPDATERT_DATO,FK_DIM_GEOGRAFI)
-#             SELECT FK_PERSON1,FK_DIM_PERSON,PERIODE,ALDER,KOMMUNE_NR,BYDEL_NR,KJONN_KODE,MAALGRUPPE_KODE
-#             ,MAALGRUPPE_NAVN,STATSBORGERSKAP,FODELAND,SIVILSTATUS_KODE,ANTBLAV,ANTBHOY,BARN_UNDER_18_ANTALL,INNTEKT_SISTE_BERAAR,INNTEKT_3_SISTE_BERAAR
-#             ,UTDSTONAD,TSOTILBARN,TSOLMIDLER,TSOBOUTG,TSODAGREIS,TSOREISOBL,TSOFLYTT,TSOREISAKT,TSOREISARB,TSOTILFAM,YBARN
-#             ,ANTBARN,ANTBU1,ANTBU3,ANTBU8,ANTBU10,ANTBU18,KILDESYSTEM,LASTET_DATO,OPPDATERT_DATO,FK_DIM_GEOGRAFI
-#             FROM dvh_fam_ef.ef_stonad_arena_final
-#         ''')
-#     cur.execute(sql)
-#     conn.commit()
-
-
 def stonad_arena_delete_insert():
     periode = get_periode()
 
@@ -65,11 +34,5 @@ def stonad_arena_delete_insert():
 
 if __name__ == "__main__":
     stonad_arena_delete_insert()
-    #periode = get_periode()
-    #conn, cur = oracle_conn()
-    #action_name = 'delete/insert into dvh_fam_ef.fam_ef_stonad_arena'
-    #send_context(conn, cur, action_name)
-    #delete_data(conn, cur, periode)
-    #insert_data(conn, cur)
-    #oracle_conn_close(conn)
+
 
--- Oracle_python/fam_ef_vedtak_arena.py
+++ Oracle_python/fam_ef_vedtak_arena.py
@@ -1,35 +1,6 @@
 from utils.db.oracle_conn import oracle_conn
 from felles_metoder import get_periode
 
-# def delete_data(conn, cur, periode):
-#     """
-#     sletter data fra fam_ef_stonad_arena med periode som kriteriea.
-#     :param periode:
-#     :return:
-#     """
-#     sql = (f"delete from dvh_fam_ef.fam_ef_vedtak_arena where periode = {periode}")
-#     cur.execute(sql)
-#     conn.commit()
-
-# def insert_data(conn, cur):
-#     """
-#     insert data fra ef_stonad_arena_final (view laget med dbt) into fam_ef_stonad_arena
-#     :param:
-#     :return:
-#     """
-#     sql = ('''
-#             INSERT INTO dvh_fam_ef.fam_ef_vedtak_arena (FK_PERSON1,PERIODE,LK_VEDTAK_ID,KOMMUNE_NR,BYDEL_NR,VEDTAK_SAK_RESULTAT_KODE,STONAD_KODE,STONADBERETT_AKTIVITET_FLAGG,AAR,SAK_STATUS_KODE,STONAD_NAVN
-#             ,MAALGRUPPE_KODE,MAALGRUPPE_NAVN,VEDTAK_DATO,VILKAAR_KODE,VILKAAR_STATUS_KODE,VILKAAR_NAVN,VEDTAK_SAK_TYPE_KODE,VEDTAK_BEHANDLING_STATUS
-#             ,GYLDIG_FRA_DATO,GYLDIG_TIL_DATO,KILDESYSTEM,LASTET_DATO)
-#             SELECT FK_PERSON1,PERIODE,LK_VEDTAK_ID,KOMMUNE_NR,BYDEL_NR,VEDTAK_SAK_RESULTAT_KODE,STONAD_KODE,STONADBERETT_AKTIVITET_FLAGG,AAR,SAK_STATUS_KODE,STONAD_NAVN
-#             ,MAALGRUPPE_KODE,MAALGRUPPE_NAVN,VEDTAK_DATO,VILKAAR_KODE,VILKAAR_STATUS_KODE,VILKAAR_NAVN,VEDTAK_SAK_TYPE_KODE,VEDTAK_BEHANDLING_STATUS
-#             ,GYLDIG_FRA_DATO,GYLDIG_TIL_DATO,KILDESYSTEM,LASTET_DATO
-#             FROM dvh_fam_ef.ef_vedtak_arena
-#         ''')
-#     cur.execute(sql)
-#     conn.commit()
-
-
 def vedtak_arena_delete_insert():
     periode = get_periode()
 
@@ -61,11 +32,5 @@ def vedtak_arena_delete_insert():
 
 if __name__ == "__main__":
     vedtak_arena_delete_insert()
-    # periode = get_periode()
-    # conn, cur = oracle_conn()
-    # action_name = 'delete/insert into dvh_fam_ef.fam_ef_vedtak_arena'
-    # send_context(conn, cur, action_name)
-    # delete_data(conn, cur, periode)
-    # insert_data(conn, cur)
-    # oracle_conn_close(conn)
+
 
