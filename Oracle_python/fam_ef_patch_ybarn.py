9d57de27341f4230359b6ce65d217a301c6f2ed5
Adam Arafa Røe <adamarafaroe@nav.no>
2023-01-02 14:03:30 +0100

    omskrev connection til db

--- Oracle_python/fam_ef_patch_ybarn.py
+++ Oracle_python/fam_ef_patch_ybarn.py
@@ -1,8 +1,20 @@
-from felles_metoder import set_secrets_as_envs, get_periode
+import datetime
 from utils.db.oracle_conn import oracle_conn
 
+
+def get_periode():
+    """
+    henter periode for the tidligere måneden eksample--> i dag er 19.04.2022, metoden vil kalkulerer periode aarMaaned eks) '202203'
+    :param periode:
+    :return: periode
+    """
+    today = datetime.date.today() # dato for idag 2022-04-19
+    first = today.replace(day=1) # dato for første dag i måneden 2022-04-01
+    lastMonth = first - datetime.timedelta(days=1) # dato for siste dag i tidligere måneden
+
+    return lastMonth.strftime("%Y%m") # henter bare aar og maaned
+
 def patch_ybarn_arena():
-    set_secrets_as_envs()
     periode = get_periode()
 
     send_context_sql = (f'''
@@ -24,13 +36,12 @@ def patch_ybarn_arena():
             FAM_EF.fam_ef_patch_infotrygd_arena (  P_IN_PERIODE_YYYYMM => P_IN_PERIODE_YYYYMM,P_OUT_ERROR => P_OUT_ERROR) ;  
             END;
         """)
-    with oracle_conn() as conn:
-        conn.autocommit = True
-        cur = conn.cursor()
+
+    conn = oracle_conn()
+    with oracle_conn().cursor() as cur:
         cur.execute(send_context_sql)
         cur.execute(sql)
-        cur.close()
-    conn.close()    
+        conn.commit()
 
 if __name__ == "__main__":
     patch_ybarn_arena()
--- Oracle_python/fam_ef_stonad_arena.py
+++ Oracle_python/fam_ef_stonad_arena.py
@@ -1,5 +1,17 @@
-from felles_metoder import get_periode
+import datetime
 from utils.db.oracle_conn import oracle_conn
+
+def get_periode():
+    """
+    henter periode for the tidligere måneden eksample--> i dag er 19.04.2022, metoden vil kalkulerer periode aarMaaned eks) '202203'
+    :param periode:
+    :return: periode
+    """
+    today = datetime.date.today() # dato for idag 2022-04-19
+    first = today.replace(day=1) # dato for første dag i måneden 2022-04-01
+    lastMonth = first - datetime.timedelta(days=1) # dato for siste dag i tidligere måneden
+
+    return lastMonth.strftime("%Y%m") # henter bare aar og maaned
 
 def stonad_arena_delete_insert():
     periode = get_periode()
@@ -26,14 +38,12 @@ def stonad_arena_delete_insert():
             FROM dvh_fam_ef.ef_stonad_arena_final
         ''')
 
-    with oracle_conn() as conn:
-        conn.autocommit = True
-        cur = conn.cursor()
+    conn = oracle_conn()
+    with conn.cursor() as cur:
         cur.execute(send_context_sql)
         cur.execute(delete_periode_sql)
         cur.execute(insert_data_sql)
-        cur.close()
-    conn.close()    
+        conn.commit()    
 
 if __name__ == "__main__":
     stonad_arena_delete_insert()
