c29dc65b6605a831c08d92c09d9a895107955c47
Adam Arafa Røe <adamarafaroe@nav.no>
2023-01-02 14:03:30 +0100

    omskrev connection til db

--- Oracle_python/fam_ef_vedtak_arena.py
+++ Oracle_python/fam_ef_vedtak_arena.py
@@ -1,5 +1,17 @@
+import datetime
 from utils.db.oracle_conn import oracle_conn
-from felles_metoder import get_periode
+
+def get_periode():
+    """
+    henter periode for the tidligere måneden eksample--> i dag er 19.04.2022, metoden vil kalkulerer periode aarMaaned eks) '202203'
+    :param periode:
+    :return: periode
+    """
+    today = datetime.date.today() # dato for idag 2022-04-19
+    first = today.replace(day=1) # dato for første dag i måneden 2022-04-01
+    lastMonth = first - datetime.timedelta(days=1) # dato for siste dag i tidligere måneden
+
+    return lastMonth.strftime("%Y%m") # henter bare aar og maaned
 
 def vedtak_arena_delete_insert():
     periode = get_periode()
@@ -23,16 +35,13 @@ def vedtak_arena_delete_insert():
             ,GYLDIG_FRA_DATO,GYLDIG_TIL_DATO,KILDESYSTEM,LASTET_DATO
             FROM dvh_fam_ef.ef_vedtak_arena
         ''')
-
-    with oracle_conn() as conn:
-        conn.autocommit = True
-        cur = conn.cursor()
+    
+    conn = oracle_conn()
+    with conn.cursor() as cur:
         cur.execute(send_context_sql)
         cur.execute(delete_periode_sql)
         cur.execute(insert_data_sql)   
-        cur.close()
-    conn.close()
-
+        conn.commit()
 
 if __name__ == "__main__":
     vedtak_arena_delete_insert()
