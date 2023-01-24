ad3dfe75484553fcadfa659b941261667966a3cb
Adam Arafa Røe <adamarafaroe@nav.no>
2023-01-24 16:51:22 +0100

    oppdatert timestamp interval navn

--- dags/kafka_test.py
+++ dags/kafka_test.py
@@ -14,8 +14,8 @@ with DAG(
   consumer = kafka_consumer_kubernetes_pod_operator(
     task_id = "hent_data_fra_topic",
     config = ks.config,
-    DATA_INTERVAL_START="1627776000000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
-    DATA_INTERVAL_END="1638316800000",   # from first day we got data until 15.11.2022 (todays before todays date)
+    data_interval_start_timestamp_milli="1627776000000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
+    data_interval_end_timestamp_milli="1638316800000",   # from first day we got data until 15.11.2022 (todays before todays date)
     slack_channel = Variable.get("slack_error_channel")
   )
 
