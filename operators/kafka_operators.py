da3e8eaa5fb2c6442a6d6cfa96e60e13e343c9b3
Adam Arafa Røe <adamarafaroe@nav.no>
2023-01-25 15:38:23 +0100

    endret image versjon

--- operators/kafka_operators.py
+++ operators/kafka_operators.py
@@ -18,7 +18,7 @@ def kafka_consumer_kubernetes_pod_operator(
     application_name: str = "dvh-airflow-kafka-consumer",
     data_interval_start_timestamp_milli: str = "{{ data_interval_start.int_timestamp * 1000 }}",
     data_interval_end_timestamp_milli: str = "{{ data_interval_end.int_timestamp * 1000 }}",
-    kafka_consumer_image: str = "ghcr.io/navikt/dvh-airflow-kafka-gcp:7f6f9f2",
+    kafka_consumer_image: str = "ghcr.io/navikt/dvh-kafka-airflow-consumer:0.4.5",
     namespace: str = os.getenv('NAMESPACE'),
     email: str = None,
     slack_channel: str = None,
