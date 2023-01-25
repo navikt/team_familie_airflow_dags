2bbb82bf6224c0731e679a3485e075ff6cc28846
Adam Arafa Røe <adamarafaroe@nav.no>
2023-01-25 14:57:18 +0100

    endret dag_id

--- dags/KS_test.py
+++ dags/KS_test.py
@@ -5,7 +5,7 @@ from kosument_config import ks
 from operators.kafka_operators import kafka_consumer_kubernetes_pod_operator
 
 with DAG(
-  dag_id = "kontantstotte_read_kafka_topic",
+  dag_id = "KS_les_data",
   start_date = datetime(2023, 1, 23),
   schedule_interval = None,#"@hourly",
   max_active_runs = 1
