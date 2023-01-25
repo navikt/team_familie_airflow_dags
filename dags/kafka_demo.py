5128ef59c7c3a15f55dbaec0aeaecdfc3b0603b4
Adam Arafa Røe <adamarafaroe@nav.no>
2023-01-25 16:26:34 +0100

    last ef

--- dags/kafka_demo.py
+++ dags/kafka_demo.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from airflow.models import DAG
 from airflow.models import Variable
-from kosument_config import test
+from kosument_config import demo
 from operators.kafka_operators import kafka_consumer_kubernetes_pod_operator
 
 with DAG(
@@ -13,9 +13,9 @@ with DAG(
 
   consumer = kafka_consumer_kubernetes_pod_operator(
     task_id = "hent_data_fra_topic",
-    config = test.config,
-    data_interval_start_timestamp_milli="1673308800000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
-    data_interval_end_timestamp_milli="1674604800000",   # from first day we got data until 15.11.2022 (todays before todays date)
+    config = demo.config,
+    data_interval_start_timestamp_milli="1672586518000", # 01.01.2023
+    data_interval_end_timestamp_milli="1674228118000",   # 20.01.2023
     slack_channel = Variable.get("slack_error_channel")
   )
 
