72e5891ca4975661e528b1d231504662e4132593
Adam Arafa Røe <adamarafaroe@nav.no>
2023-01-25 11:35:24 +0100

    laster KS data hourly og laster en EF last

--- dags/kafka_test.py
+++ dags/kafka_test.py
@@ -14,8 +14,8 @@ with DAG(
   consumer = kafka_consumer_kubernetes_pod_operator(
     task_id = "hent_data_fra_topic",
     config = test.config,
-    data_interval_start_timestamp_milli="1661990400000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
-    data_interval_end_timestamp_milli="1674172800000",   # from first day we got data until 15.11.2022 (todays before todays date)
+    data_interval_start_timestamp_milli="1627776000000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
+    data_interval_end_timestamp_milli="1646092800000",   # from first day we got data until 15.11.2022 (todays before todays date)
     slack_channel = Variable.get("slack_error_channel")
   )
 
