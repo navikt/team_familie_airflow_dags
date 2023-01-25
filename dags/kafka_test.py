917e1cb04da04cd100484e43c64c8bbb81cc7259
Adam Arafa Røe <adamarafaroe@nav.no>
2023-01-25 08:50:07 +0100

    henter data fra desember

--- dags/kafka_test.py
+++ dags/kafka_test.py
@@ -14,7 +14,7 @@ with DAG(
   consumer = kafka_consumer_kubernetes_pod_operator(
     task_id = "hent_data_fra_topic",
     config = ks.config,
-    data_interval_start_timestamp_milli="1661990400000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
+    data_interval_start_timestamp_milli="1669852800000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
     data_interval_end_timestamp_milli="1674172800000",   # from first day we got data until 15.11.2022 (todays before todays date)
     slack_channel = Variable.get("slack_error_channel")
   )
