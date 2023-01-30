b6b5541614952e140d55c9ddbadb1f3749cf2642
Adam Arafa Røe <adamarafaroe@nav.no>
2023-01-30 08:57:30 +0100

    henter KS data last

--- dags/kontantstotte_kafka.py
+++ dags/kontantstotte_kafka.py
@@ -6,16 +6,16 @@ from operators.kafka_operators import kafka_consumer_kubernetes_pod_operator
 
 with DAG(
   dag_id="kontantstotte_read_kafka_topic",
-  start_date=datetime(2022, 12, 15),
-  schedule_interval= "@hourly",
+  start_date=datetime(2023, 1, 29),
+  schedule_interval= None,#"@hourly",
   max_active_runs=1
 ) as dag:
 
   consumer = kafka_consumer_kubernetes_pod_operator(
     task_id = "kontantstotte_hent_kafka_data",
     config = ks.config,
-    #data_interval_start_timestamp_milli="1667260800000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
-    #data_interval_end_timestamp_milli="1671062400000",   # from first day we got data until 15.11.2022 (todays before todays date)
+    data_interval_start_timestamp_milli="1667260800000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
+    data_interval_end_timestamp_milli="1674950400000",   # from first day we got data until 15.11.2022 (todays before todays date)
     slack_channel = Variable.get("slack_error_channel")
   )
 
