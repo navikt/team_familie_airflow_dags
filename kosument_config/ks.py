c25c211d4bdf9a74565671666eff7bf51d7360be
AArafaR <97885051+AArafaR@users.noreply.github.com>
2023-01-31 11:31:30 +0100

    nytt image og endret i configen

--- kosument_config/ks.py
+++ kosument_config/ks.py
@@ -10,7 +10,7 @@ target:
   type: oracle
   custom-config:
     - method: oracledb.Cursor.setinputsizes
-      name: kafka_message
+      name: melding
       value: oracledb.BLOB
   skip-duplicates-with: 
     - kafka_offset
--- operators/kafka_operators.py
+++ operators/kafka_operators.py
@@ -18,7 +18,7 @@ def kafka_consumer_kubernetes_pod_operator(
     application_name: str = "dvh-airflow-kafka-consumer",
     data_interval_start_timestamp_milli: str = "{{ data_interval_start.int_timestamp * 1000 }}",
     data_interval_end_timestamp_milli: str = "{{ data_interval_end.int_timestamp * 1000 }}",
-    kafka_consumer_image: str = "ghcr.io/navikt/dvh-airflow-kafka-gcp:b7d7a6c",
+    kafka_consumer_image: str = "ghcr.io/navikt/dvh-airflow-kafka-gcp:7fbb055",
     namespace: str = os.getenv('NAMESPACE'),
     email: str = None,
     slack_channel: str = None,
