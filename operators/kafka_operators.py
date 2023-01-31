2cf5f7b641ba45c24efbf5311155cafceab06804
AArafaR <97885051+AArafaR@users.noreply.github.com>
2023-01-31 11:34:50 +0100

    from resources til container_resources i kafka_operators

--- operators/kafka_operators.py
+++ operators/kafka_operators.py
@@ -84,7 +84,7 @@ def kafka_consumer_kubernetes_pod_operator(
         volume_mounts=[vault_volume_mount()],
         service_account_name=os.getenv('TEAM'),
         annotations={"sidecar.istio.io/inject": "false"},
-        resources=client.V1ResourceRequirements(
+        container_resources=client.V1ResourceRequirements(
             requests={"memory": "4G"},
             limits={"memory": "4G"}
         ),
