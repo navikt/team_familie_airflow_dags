23c6ac41dbcb74726f26f676d366216414a38741
AArafaR <97885051+AArafaR@users.noreply.github.com>
2023-04-20 13:37:43 +0200

    pakket stringen i fnotter i insert_test_data

--- Oracle_python/insert_test_data.py
+++ Oracle_python/insert_test_data.py
@@ -4,12 +4,11 @@ from felles_metoder.felles_metoder import oracle_secrets
 def opprett_oracle_tabell():
     secrets = oracle_secrets()
 
-    insert_data_sql = ("""
-                        INSERT INTO dvh_fam_ef.test_tabell (id, navn)
-                        VALUES (1,  Kjell-Tore)
+    insert_data_sql = ("""INSERT INTO dvh_fam_ef.test_tabell (id, navn)
+                        VALUES (1,  'Kjell-Tore')
                         """)
 
-    dsn_tns = cx_Oracle.makedsn(secrets['host']['dvh_fam_ef'], 1521, service_name = secrets['service'])
+    dsn_tns = cx_Oracle.makedsn(secrets['host'], 1521, service_name = secrets['service'])
     with cx_Oracle.connect(user = secrets['user']['dvh_fam_ef'], password = secrets['password'], dsn = dsn_tns) as connection:
         with connection.cursor() as cursor:
             cursor.execute(insert_data_sql)
