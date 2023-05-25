73bfe42ef2655f8d3f6756a54eba40277e76c24c
AArafaR <97885051+AArafaR@users.noreply.github.com>
2023-05-25 14:28:27 +0200

    opprette en dag og en config fil for å konsumere bt med airflow

---
+++ dags/barnetryg_kafka.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from airflow.models import DAG
+from airflow.models import Variable
+from kosument_config import barnetrygd
+from operators.kafka_operators import kafka_consumer_kubernetes_pod_operator
+
+with DAG(
+  dag_id="barnetrygd_read_kafka_topic",
+  start_date=datetime(2023, 5, 23),
+  schedule_interval= None, #"@hourly",
+  max_active_runs=1
+) as dag:
+
+  consumer = kafka_consumer_kubernetes_pod_operator(
+    task_id = "barnetrygd_hent_kafka_data",
+    config = barnetrygd.config,
+    data_interval_start_timestamp_milli="1684022400000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
+    data_interval_end_timestamp_milli="1684972800000",   # from first day we got data until 15.11.2022 (todays before todays date)
+    slack_channel = Variable.get("slack_error_channel")
+  )
+
+consumer
+
