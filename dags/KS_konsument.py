d6b93e4c7b7a501127e299d79b49dbfe309fcdcd
AArafaR <97885051+AArafaR@users.noreply.github.com>
2023-07-17 11:50:45 +0200

    lagt til catchup = False i KS dagen

--- dags/KS_konsument.py
+++ dags/KS_konsument.py
@@ -22,7 +22,8 @@ with DAG(
   dag_id="KS_konsument",
   start_date=datetime(2023, 7, 17),
   schedule_interval= "@hourly",
-  max_active_runs=1
+  max_active_runs=1,
+  catchup = False
 ) as dag:
 
   consumer = kafka_consumer_kubernetes_pod_operator(
