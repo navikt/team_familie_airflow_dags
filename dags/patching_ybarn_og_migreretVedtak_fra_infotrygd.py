6c1d1913463fe8cf5950dae8554d44462c77bc4b
AArafaR <97885051+AArafaR@users.noreply.github.com>
2023-07-25 11:04:31 +0200

    opprettet en ny python fil for å kalle procedure fam_ef_patch_migrerte_vedtak og oppdatert og lagt til en ny task i dag patching ybarn

--- dags/patching_ybarn_og_migreretVedtak_fra_infotrygd.py
+++ dags/patching_ybarn_og_migreretVedtak_fra_infotrygd.py
@@ -1,7 +1,6 @@
 from airflow.models import DAG, Variable
 from airflow.utils.dates import datetime, timedelta
 from kubernetes import client
-import os
 from operators.slack_operator import slack_info, slack_error
 from airflow.decorators import task
 from dataverk_airflow.knada_operators import create_knada_python_pod_operator
@@ -15,19 +14,18 @@ default_args = {
     }
 
 with DAG(
-    dag_id = 'Fam_EF_patching_ybarn_arena', 
-    description = 'An Airflow DAG that invokes "FAM_EF.fam_ef_patch_infotrygd_arena" stored procedure',
+    dag_id = 'Fam_EF_patching_ybarn_og_migrerte_vedtak', 
+    description = 'An Airflow DAG that invokes "FAM_EF.fam_ef_patch_infotrygd_arena" og "FAM_EF.fam_ef_patch_migrering_vedtak" stored procedures',
     default_args = default_args,
-    start_date = datetime(2022, 10, 1), # start date for the dag
-    schedule_interval = None,#'0 0 5 * *' , # 5te hver måned,
+    start_date = datetime(2023, 7, 25), # start date for the dag
+    schedule_interval = '0 0 5 * *' , # 5te hver måned,
     catchup = False # makes only the latest non-triggered dag runs by airflow (avoid having all dags between start_date and current date running
 ) as dag:
-
 
     @task
     def notification_start():
         slack_info(
-            message = "Patching av ybarn til fam_ef_stonad tabellen starter nå ved hjelp av Airflow! :rocket:"
+            message = "Patching av ybarn og migrerte_vedtak starter nå ved hjelp av Airflow! :rocket:"
         )
 
     start_alert = notification_start()
@@ -37,17 +35,29 @@ with DAG(
         name="fam_ef_patch_ybarn_infotrygd_arena",
         repo="navikt/team_familie_airflow_dags",
         script_path="Oracle_python/fam_ef_patch_ybarn.py",
-        branch="test_r",
+        branch=Variable.get("branch"),
         resources=client.V1ResourceRequirements(
             requests={"memory": "4G"},
             limits={"memory": "4G"}),
         slack_channel=Variable.get("slack_error_channel")
     )
 
+    patch_migrerte_vedtak = create_knada_python_pod_operator(
+    dag=dag,
+    name="fam_ef_patch_migrerte_vedtak",
+    repo="navikt/team_familie_airflow_dags",
+    script_path="Oracle_python/fam_ef_patch_migrerte_vedtak.py",
+    branch=Variable.get("branch"),
+    resources=client.V1ResourceRequirements(
+        requests={"memory": "4G"},
+        limits={"memory": "4G"}),
+    slack_channel=Variable.get("slack_error_channel")
+    )
+
     @task
     def notification_end():
         slack_info(
-            message = "Fam_Ef_patch_ybarn_infotrygd_arena er nå ferdig kjørt! :tada: :tada:"
+            message = "Patching av ybarn og migrerte_vedtak er nå ferdig kjørt! :tada: :tada:"
         )
     slutt_alert = notification_end()
 
