c95343bbe24684746c65f3eeda439ba335fa2f0b
AArafaR <97885051+AArafaR@users.noreply.github.com>
2023-07-31 12:23:01 +0200

    kjører dagen hver time

--- dags/BT_konsument.py
+++ dags/BT_konsument.py
@@ -20,7 +20,7 @@ v_schema = settings["schema"]
 with DAG(
   dag_id="BT_konsument",
   start_date=datetime(2023, 7, 17),
-  schedule_interval= None, #"@hourly",
+  schedule_interval= "@hourly",
   max_active_runs=1,
   catchup = False
 ) as dag:
@@ -28,19 +28,19 @@ with DAG(
   consumer = kafka_consumer_kubernetes_pod_operator(
     task_id = "barnetrygd_hent_kafka_data",
     config = barnetrygd.config,
-    data_interval_start_timestamp_milli="1690549200000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
-    data_interval_end_timestamp_milli="1690786800000",   # from first day we got data until 29.05.2023 (todays before todays date)
+    #data_interval_start_timestamp_milli="1684022400000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
+    #data_interval_end_timestamp_milli="1685318400000",   # from first day we got data until 29.05.2023 (todays before todays date)
     slack_channel = Variable.get("slack_error_channel")
   )
 
-  # bt_utpakking_dbt = create_dbt_operator(
-  #     dag=dag,
-  #     name="utpakking_bt",
-  #     script_path = 'airflow/dbt_run.py',
-  #     branch=v_branch,
-  #     dbt_command= """run --select BT_utpakking.* --vars "{{dag_interval_start: '{0}', dag_interval_end: '{1}'}}" """.format('{{ execution_date.in_timezone("Europe/Amsterdam").strftime("%Y-%m-%d %H:%M:%S")}}','{{ (execution_date + macros.timedelta(hours=1)).in_timezone("Europe/Amsterdam").strftime("%Y-%m-%d %H:%M:%S")}}'),
-  #     db_schema=v_schema
-  # )
+  bt_utpakking_dbt = create_dbt_operator(
+      dag=dag,
+      name="utpakking_bt",
+      script_path = 'airflow/dbt_run.py',
+      branch=v_branch,
+      dbt_command= """run --select BT_utpakking.* --vars "{{dag_interval_start: '{0}', dag_interval_end: '{1}'}}" """.format('{{ execution_date.in_timezone("Europe/Amsterdam").strftime("%Y-%m-%d %H:%M:%S")}}','{{ (execution_date + macros.timedelta(hours=1)).in_timezone("Europe/Amsterdam").strftime("%Y-%m-%d %H:%M:%S")}}'),
+      db_schema=v_schema
+  )
 
-consumer #>> bt_utpakking_dbt
+consumer >> bt_utpakking_dbt
 
