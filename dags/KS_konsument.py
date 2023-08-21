cd907ac7a43a2e0faf655c58ab8da0477ff745c5
AArafaR <97885051+AArafaR@users.noreply.github.com>
2023-08-21 10:10:32 +0200

    konsument dagene henter topic navn fra airflow variabler

--- dags/KS_konsument.py
+++ dags/KS_konsument.py
@@ -6,7 +6,6 @@ from operators.kafka_operators import kafka_consumer_kubernetes_pod_operator
 from operators.dbt_operator import create_dbt_operator
 from operators.slack_operator import slack_error
 
-
 default_args = {
     'owner': 'Team-Familie',
     'retries': 1,
@@ -18,6 +17,8 @@ settings = Variable.get("dbt_ks_schema", deserialize_json=True)
 v_branch = settings["branch"]
 v_schema = settings["schema"]
 
+topic = Variable.get("KS_topic") 
+
 with DAG(
   dag_id="KS_konsument",
   start_date=datetime(2023, 7, 17),
@@ -28,7 +29,7 @@ with DAG(
 
   consumer = kafka_consumer_kubernetes_pod_operator(
     task_id = "kontantstotte_hent_kafka_data",
-    config = ks.config,
+    config = ks.config.format(topic),
     #data_interval_start_timestamp_milli="1659312000000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
     #data_interval_end_timestamp_milli="1675209600000",   # from first day we got data until 15.11.2022 (todays before todays date)
     slack_channel = Variable.get("slack_error_channel")
