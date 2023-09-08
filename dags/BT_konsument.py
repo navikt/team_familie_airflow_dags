3b3781b152a159fe46db9bf197939bdf4120cf1f
AArafaR <97885051+AArafaR@users.noreply.github.com>
2023-09-08 10:52:06 +0200

    bruker defualt_args i dagen for å sende en slack varsel hvis dagen feiler

--- dags/BT_konsument.py
+++ dags/BT_konsument.py
@@ -22,9 +22,10 @@ topic = Variable.get("BT_topic")
 with DAG(
   dag_id="BT_konsument",
   start_date=datetime(2023, 7, 17),
+  default_args = default_args,
   schedule_interval= "@hourly",
   max_active_runs=1,
-  catchup = False
+  catchup = True
 ) as dag:
 
   consumer = kafka_consumer_kubernetes_pod_operator(
