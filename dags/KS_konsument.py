e4abe0247dfd8ac2fec9a3df58aef62bf321ff2c
AArafaR <97885051+AArafaR@users.noreply.github.com>
2023-09-25 10:24:18 +0200

    kjører en ny ks last

--- dags/KS_konsument.py
+++ dags/KS_konsument.py
@@ -30,7 +30,7 @@ with DAG(
     task_id = "kontantstotte_hent_kafka_data",
     config = ks.config,
     data_interval_start_timestamp_milli="1682899200000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
-    data_interval_end_timestamp_milli="1683244800000",   # from first day we got data until 15.11.2022 (todays before todays date)
+    data_interval_end_timestamp_milli="1695168000000",   # from first day we got data until 15.11.2022 (todays before todays date)
     slack_channel = Variable.get("slack_error_channel")
   )
 
