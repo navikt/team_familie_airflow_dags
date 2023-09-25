f9f5fd9d0e3d37f8a663f9f9f5aed8efb613171f
AArafaR <97885051+AArafaR@users.noreply.github.com>
2023-09-25 12:09:50 +0200

    kjører en KS last fra 1 juni til 25 september

--- dags/KS_konsument.py
+++ dags/KS_konsument.py
@@ -29,8 +29,8 @@ with DAG(
   consumer = kafka_consumer_kubernetes_pod_operator(
     task_id = "kontantstotte_hent_kafka_data",
     config = ks.config,
-    data_interval_start_timestamp_milli="1682899200000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
-    data_interval_end_timestamp_milli="1683244800000",   # from first day we got data until 15.11.2022 (todays before todays date)
+    data_interval_start_timestamp_milli="1685577600000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
+    data_interval_end_timestamp_milli="1695646800000",   # from first day we got data until 15.11.2022 (todays before todays date)
     slack_channel = Variable.get("slack_error_channel")
   )
 
