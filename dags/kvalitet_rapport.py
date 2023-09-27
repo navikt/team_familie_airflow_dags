d4b9aeea5f270d3a428840bce9f80ac571b49d65
AArafaR <97885051+AArafaR@users.noreply.github.com>
2023-09-27 11:01:49 +0200

    fjernet position [0] fra alle sql som sjekker om det er hull i  meta_data eller ikke

--- dags/kvalitet_rapport.py
+++ dags/kvalitet_rapport.py
@@ -62,11 +62,11 @@ with DAG(
     """
     with oracle_conn().cursor() as cur:
         bt_ant = cur.execute(bt_ant_mottatt_mldinger).fetchone()[0]
-        bt_hull = cur.execute(sjekk_hull_i_BT_meta_data).fetchone()[0]
+        bt_hull = cur.execute(sjekk_hull_i_BT_meta_data).fetchone()
         ef_ant = cur.execute(ef_ant_mottatt_mldinger).fetchone()[0]
-        ef_hull = cur.execute(sjekk_hull_i_EF_meta_data).fetchone()[0]
+        ef_hull = cur.execute(sjekk_hull_i_EF_meta_data).fetchone()
         ks_ant = cur.execute(ks_ant_mottatt_mldinger).fetchone()[0]
-        ks_hull = cur.execute(sjekk_hull_i_KS_meta_data).fetchone()[0]
+        ks_hull = cur.execute(sjekk_hull_i_KS_meta_data).fetchone()
         pp_ant = cur.execute(pp_ant_mottatt_mldinger).fetchone()[0]
     return [bt_ant,bt_hull,ef_ant,ef_hull,ks_ant,ks_hull,pp_ant]
 
