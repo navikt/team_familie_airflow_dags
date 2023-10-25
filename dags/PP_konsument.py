12d95478f9b3d89fcd6f598decdf0bb09323e56b
AArafaR <97885051+AArafaR@users.noreply.github.com>
2023-10-25 16:35:10 +0200

    kjører en pp last

--- dags/PP_konsument.py
+++ dags/PP_konsument.py
@@ -28,7 +28,7 @@ with DAG(
   consumer = kafka_consumer_kubernetes_pod_operator(
     task_id = "pleiepenger_hent_kafka_data",
     config = pp.config,
-    data_interval_start_timestamp_milli="1698231600000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
+    data_interval_start_timestamp_milli="1698224400000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
     data_interval_end_timestamp_milli="1698249600000",   # from first day we got data until 29.05.2023 (todays before todays date)
     slack_channel = Variable.get("slack_error_channel")
   )
