43cc7c01ff80643d70ffdc8cc739379a6a641ca2
AArafaR <97885051+AArafaR@users.noreply.github.com>
2023-10-26 08:47:38 +0200

    adda PP info i kvalitet_rapport

--- dags/kvalitet_rapport.py
+++ dags/kvalitet_rapport.py
@@ -27,6 +27,9 @@ with DAG(
     ks_ant_mottatt_mldinger = """
       SELECT COUNT(*) FROM DVH_FAM_KS.fam_ks_meta_data WHERE lastet_dato >= sysdate - 1
     """
+    pp_ant_mottatt_mldinger = """
+      SELECT COUNT(*) FROM DVH_FAM_PP.fam_pp_meta_data WHERE lastet_dato >= sysdate - 1
+    """
     sjekk_hull_i_BT_meta_data = """
         SELECT * FROM
             (SELECT lastet_dato, kafka_topic, kafka_offset,
@@ -54,6 +57,15 @@ with DAG(
             FROM DVH_FAM_KS.fam_ks_meta_data)
         where neste-kafka_offset > 1 and lastet_dato > to_date('27.09.2023', 'dd.mm.yyyy')
     """
+    sjekk_hull_i_PP_meta_data = """
+        SELECT * FROM
+            (SELECT lastet_dato, kafka_topic, kafka_offset,
+                LEAD(kafka_offset) 
+                OVER(PARTITION BY kafka_topic
+                ORDER BY kafka_offset) neste
+            FROM DVH_FAM_PP.fam_pp_meta_data)
+        where neste-kafka_offset > 1 and lastet_dato > to_date('24.10.2023', 'dd.mm.yyyy')
+    """
     with oracle_conn().cursor() as cur:
         bt_ant = cur.execute(bt_ant_mottatt_mldinger).fetchone()[0]
         bt_hull = [str(x) for x in (cur.execute(sjekk_hull_i_BT_meta_data).fetchone() or [])]
@@ -61,7 +73,9 @@ with DAG(
         ef_hull = [str(x) for x in (cur.execute(sjekk_hull_i_EF_meta_data).fetchone() or [])]
         ks_ant = cur.execute(ks_ant_mottatt_mldinger).fetchone()[0]
         ks_hull = [str(x) for x in (cur.execute(sjekk_hull_i_KS_meta_data).fetchone() or [])]
-    return [bt_ant,bt_hull,ef_ant,ef_hull,ks_ant,ks_hull]
+        pp_ant = cur.execute(pp_ant_mottatt_mldinger).fetchone()[0]
+        pp_hull = [str(x) for x in (cur.execute(sjekk_hull_i_PP_meta_data).fetchone() or [])]       
+    return [bt_ant,bt_hull,ef_ant,ef_hull,ks_ant,ks_hull,pp_ant,pp_hull]
 
 
   @task
@@ -71,6 +85,7 @@ with DAG(
       bt_ant,bt_hull,
       ef_ant,ef_hull,
       ks_ant,ks_hull,
+      pp_ant,pp_hull,     
     ] = kafka_last
     bt_antall_meldinger = f"Antall mottatt BT meldinger for {gaarsdagensdato}......................{str(bt_ant)}"
     bt_hull_i_meta_data = f"Manglene kafka_offset i BT_meta_data for {gaarsdagensdato}:............{str(bt_hull)}"
@@ -78,10 +93,14 @@ with DAG(
     ef_hull_i_meta_data = f"Manglene kafka_offset i BT_meta_data for {gaarsdagensdato}:............{str(ef_hull)}"
     ks_antall_meldinger = f"Antall mottatt KS meldinger for {gaarsdagensdato}......................{str(ks_ant)}"
     ks_hull_i_meta_data = f"Manglene kafka_offset i BT_meta_data for {gaarsdagensdato}:............{str(ks_hull)}"
+    pp_antall_meldinger = f"Antall mottatt PP meldinger for {gaarsdagensdato}......................{str(pp_ant)}"
+    pp_hull_i_meta_data = f"Manglene kafka_offset i PP_meta_data for {gaarsdagensdato}:............{str(pp_hull)}"
     konsumenter_summary = f"""
 *Leste meldinger fra konsumenter siste døgn:*
  
 ```
+{pp_antall_meldinger}
+{pp_hull_i_meta_data}
 {bt_antall_meldinger}
 {bt_hull_i_meta_data}
 {ef_antall_meldinger}
