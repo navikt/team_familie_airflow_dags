199ed9d747e982dde1aa9beea7a43e611fc6372c
AArafaR <97885051+AArafaR@users.noreply.github.com>
2023-11-07 12:56:43 +0100

    lagt til et image i BS notebook_operator

--- dags/brillestonad_data_migrering.py
+++ dags/brillestonad_data_migrering.py
@@ -36,7 +36,7 @@ with DAG(
         requests={'memory': '4G'},
         limits={'memory': '4G'}),
     slack_channel = Variable.get('slack_error_channel'),
-    requirements_path="requirements.txt",
+    image='ghcr.io/navikt/dvh-images/airflow-dbt:2023-11-01-1482dbf',
     log_output=False
     )
 
