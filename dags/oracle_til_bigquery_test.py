b5f42523298a74d76cbce2d9544675c36dd9e591
Helen Rong <helen.rong@nav.no>
2023-11-30 15:09:51 +0100

    Lagt til skjemanavn

--- dags/oracle_til_bigquery_test.py
+++ dags/oracle_til_bigquery_test.py
@@ -6,7 +6,7 @@ with DAG('SimpleOracleToBigqueryOperator', start_date=datetime(2023, 2, 14), sch
 
     oracle_to_bq = oracle_to_bigquery(
         oracle_con_id="oracle_con",
-        oracle_table="FAM_ORACLE_BIGQUERY_TEST",
+        oracle_table="DVH_FAM_FP.FAM_ORACLE_BIGQUERY_TEST",
         gcp_con_id="google_con_different_project",
         bigquery_dest_uri="dv-familie-dev-f48b.test.fra_oracle_fp",
     )
