e25d59027f21fd3d089c31539c8bebf6013b92bc
Helen Rong <helen.rong@nav.no>
2023-11-30 15:11:07 +0100

    Slettet migrert fra patch_ybarn dag

--- dags/patching_ybarn_og_migreretVedtak_fra_infotrygd.py
+++ dags/patching_ybarn_og_migreretVedtak_fra_infotrygd.py
@@ -51,5 +51,5 @@ with DAG(
         )
     slutt_alert = notification_end()
 
-start_alert >> patch_migrerte_vedtak >> patch_ybarn_arena >> slutt_alert
+start_alert >> patch_ybarn_arena >> slutt_alert
 
