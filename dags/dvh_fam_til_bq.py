d1b06b6892672a675947fb63767410487de2bba6
Helen Rong <helen.rong@nav.no>
2023-12-11 14:51:00 +0100

    Lagt til oracle_table i oracle_to_bigquery

--- dags/dvh_fam_til_bq.py
+++ dags/dvh_fam_til_bq.py
@@ -59,7 +59,7 @@ def oracle_to_bigquery(
 with DAG('DVH_FAM_Til_BigQuery', start_date=datetime(2023, 11, 29), schedule=None) as dag:
     agg_fam_stonad_ur = oracle_to_bigquery(
         oracle_con_id="oracle_con",
-        oracle_table= f"{tabellnavn}.{schema}", # oracle_table hentes fra airflow->admin->variables. Det går sjappere å endre tabellnavn der enn å gjøre det i selv dagen!    "agg_fam_bt_eos_kpi", 
+        oracle_table= f"{schema}.{tabellnavn}", # oracle_table hentes fra airflow->admin->variables. Det går sjappere å endre tabellnavn der enn å gjøre det i selv dagen!    "agg_fam_bt_eos_kpi", 
         gcp_con_id="google_con_different_project",
         bigquery_dest_uri=f"dv-familie-prod-17e7.dvh_fam.{tabellnavn}",
     )
