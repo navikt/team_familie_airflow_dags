ed955a2a8e6e15e58ad992db2683883d4075d314
AArafaR <97885051+AArafaR@users.noreply.github.com>
2024-01-09 12:45:18 +0100

    slack i nb operator i BS

--- operators/slack_operator.py
+++ operators/slack_operator.py
@@ -38,7 +38,7 @@ def __slack_message(
     channel=channel,
     executor_config={
       "pod_override": k8s.V1Pod(
-          metadata=k8s.V1ObjectMeta(annotations={"allowlist": 'slack.com,hooks.slack.com'})
+          metadata=k8s.V1ObjectMeta(annotations={"allowlist": "slack.com,hooks.slack.com"})
       )
         }
   ).execute()
