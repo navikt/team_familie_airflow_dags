8191db85d0b57a29d39302163a15e50fd0970cb5
AArafaR <97885051+AArafaR@users.noreply.github.com>
2024-02-20 15:39:44 +0100

    endret host navn for U basen

--- allowlists/allowlist.py
+++ allowlists/allowlist.py
@@ -1,7 +1,7 @@
 slack_allowlist = ['slack.com','hooks.slack.com']
 
 prod_oracle_conn_id = ['dm09-scan.adeo.no:1521']
-dev_oracle_conn_id = ['dm07-scan.adeo.no:1521']
+dev_oracle_conn_id = ['dmv07-scan.adeo.no:1521']
 
 prod_aiven_conn_ip = ['nav-prod-kafka-nav-prod.aivencloud.com:26484', 'nav-prod-kafka-nav-prod.aivencloud.com:26487']
 dev_aiven_conn_ip = ['nav-dev-kafka-nav-dev.aivencloud.com:26484', 'nav-dev-kafka-nav-dev.aivencloud.com:26487']
