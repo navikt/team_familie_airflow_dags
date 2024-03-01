5e693eda8880dd2d96924e64096b27167f7a35d7
Helen Rong <helen.rong@nav.no>
2024-03-01 15:15:56 +0100

    Endret FP tilbake til hourly

--- dags/FP_konsument.py
+++ dags/FP_konsument.py
@@ -42,8 +42,8 @@ with DAG(
   consumer = kafka_consumer_kubernetes_pod_operator(
     task_id = "foreldrepenger_hent_kafka_data",
     config = fp.config.format(topic),
-    data_interval_start_timestamp_milli="1708955634000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
-    data_interval_end_timestamp_milli="1709301234000",   # from first day we got data until 29.05.2023 (todays before todays date)
+    #data_interval_start_timestamp_milli="1708955634000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
+    #data_interval_end_timestamp_milli="1709301234000",   # from first day we got data until 29.05.2023 (todays before todays date)
     slack_channel = Variable.get("slack_error_channel")
   )
 
