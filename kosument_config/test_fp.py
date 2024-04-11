5721369c306d71960bd8ecff64e9ae27a797a916
Helen Rong <helen.rong@nav.no>
2024-04-11 10:24:44 +0200

    Test

---
+++ kosument_config/test_fp.py
@@ -0,0 +1,34 @@
+config="""
+source:
+  type: kafka
+  batch-size: 5000
+  batch-interval: 5
+  topic: {}
+  schema: json
+  keypath-seperator: /
+target:
+  type: oracle
+  custom-config:
+    - method: oracledb.Cursor.setinputsizes
+      name: melding
+      value: oracledb.BLOB
+  skip-duplicates-with: 
+    - kafka_offset
+    - kafka_topic
+    - kafka_partition
+  table: DVH_FAM_FP.TEST_FAM_FP_META_DATA
+transform:
+  - src: kafka_message
+    dst: melding
+  - src: kafka_topic
+    dst: kafka_topic
+  - src: kafka_offset
+    dst: kafka_offset
+  - src: kafka_timestamp
+    dst: kafka_mottatt_dato
+    fun: int-unix-ms -> datetime-no
+  - src: kafka_partition
+    dst: kafka_partition
+  - src: $$$BATCH_TIME
+    dst: lastet_dato
+"""
