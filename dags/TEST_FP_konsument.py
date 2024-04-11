78817999a232c334bd07bc144acd267e267ebd5f
Helen Rong <helen.rong@nav.no>
2024-04-11 15:46:55 +0200

    Test ny image

--- dags/TEST_FP_konsument.py
+++ dags/TEST_FP_konsument.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import os
 from airflow.models import DAG
 from airflow.models import Variable
 from kosument_config import test_fp
@@ -44,8 +45,8 @@ with DAG(
     #data_interval_end_timestamp_milli="1698156000000",   # from first day we got data until 29.05.2023 (todays before todays date)
     slack_channel = Variable.get("slack_error_channel"),
     extra_envs={
-            "SOURCE_SECRET_PATH": f"projects/{Variable.get('TEAM_GCP_PROJECT')}/secrets/team-familie-ptdc/versions/latest",
-            "TARGET_SECRET_PATH": f"projects/{Variable.get('TEAM_GCP_PROJECT')}/secrets/team-familie-ptdc/versions/latest",
+            "SOURCE_SECRET_PATH": f"{os.environ['KNADA_TEAM_SECRET']}/versions/latest",
+            "TARGET_SECRET_PATH": f"{os.environ['KNADA_TEAM_SECRET']}/versions/latest",
         },
     kafka_consumer_image = "ghcr.io/navikt/dvh-airflow-kafka:2024-04-10-8310685",
   )
