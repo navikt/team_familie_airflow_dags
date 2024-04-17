f2c9c0d907c657b5a4651b01a18f0bb333cd7f8f
AArafaR <97885051+AArafaR@users.noreply.github.com>
2024-04-17 08:36:30 +0200

    tester med et nytt gammelt image

--- operators/kafka_operators.py
+++ operators/kafka_operators.py
@@ -16,7 +16,7 @@ def kafka_consumer_kubernetes_pod_operator(
     application_name: str = "dvh-airflow-kafka-consumer",
     data_interval_start_timestamp_milli: str = "{{ data_interval_start.int_timestamp * 1000 }}",
     data_interval_end_timestamp_milli: str = "{{ data_interval_end.int_timestamp * 1000 }}",
-    kafka_consumer_image: str = "ghcr.io/navikt/dvh-airflow-kafka:35172e9",					
+    kafka_consumer_image: str = "ghcr.io/navikt/dvh-airflow-kafka:da01735",					
     namespace: str = os.getenv('NAMESPACE'),
     email: str = None,
     slack_channel: str = None,
