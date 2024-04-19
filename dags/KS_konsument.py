445573720945e09e6b53471b2600eb832b9fde56
AArafaR <97885051+AArafaR@users.noreply.github.com>
2024-04-19 09:55:34 +0200

    henter manglenede kafka_offset på KS og PP

--- dags/KS_konsument.py
+++ dags/KS_konsument.py
@@ -32,7 +32,7 @@ with DAG(
   dag_id="KS_konsument",
   start_date=datetime(2024, 4, 18, 12),
   default_args = default_args,
-  schedule_interval= "@hourly",
+  schedule_interval= None, #"@hourly",
   max_active_runs=1,
   catchup = True
 ) as dag:
@@ -40,8 +40,8 @@ with DAG(
   consumer = kafka_consumer_kubernetes_pod_operator(
     task_id = "kontantstotte_hent_kafka_data",
     config = ks.config.format(topic),
-    #data_interval_start_timestamp_milli="1712916000000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
-    #data_interval_end_timestamp_milli="1713448800000",   # from first day we got data until 29.05.2023 (todays before todays date)
+    data_interval_start_timestamp_milli="1713438000000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
+    data_interval_end_timestamp_milli="1713448800000",   # from first day we got data until 29.05.2023 (todays before todays date)
     slack_channel = Variable.get("slack_error_channel")
   )
 
