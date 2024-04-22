f8fd7214f6158e64c3d56425b641c03a9843f992
AArafaR <97885051+AArafaR@users.noreply.github.com>
2024-04-22 14:19:50 +0200

    tester en custom slack operator

--- dags/slack_link_test.py
+++ dags/slack_link_test.py
@@ -1,13 +1,9 @@
-from datetime import datetime
-from datetime import date
-from datetime import timedelta
 from airflow import DAG
+from datetime import datetime
+from operators import CustomSlackOperator
 from airflow.models import Variable
 from airflow.decorators import task
 from kubernetes import client
-from operators.slack_operator import slack_error, slack_info
-from airflow.providers.slack.operators.slack import SlackAPIPostOperator
-from utils.db.oracle_conn import oracle_conn
 from allowlists.allowlist import prod_oracle_slack, dev_oracle_slack, r_oracle_slack
 
 miljo = Variable.get('miljo')   
@@ -20,25 +16,26 @@ elif miljo == 'test_r':
 else:
     allowlist.extend(dev_oracle_slack)
 
-with DAG(
-  dag_id='slack_link_test',
-  default_args={'on_failure_callback': slack_error},
-  start_date=datetime(2024, 4, 22),
-  schedule_interval= None, 
-  catchup=False
-) as dag:
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 4, 19),
+}
 
-    @task(
-         executor_config={
-            "pod_override": client.V1Pod(
-               metadata=client.V1ObjectMeta(annotations={"allowlist":  ",".join(allowlist)})
-            )
-         }
+dag = DAG('slack_link_test', default_args=default_args, schedule_interval=None)
+
+@task(
+    executor_config={
+        "pod_override": client.V1Pod(
+            metadata=client.V1ObjectMeta(annotations={"allowlist":  ",".join(allowlist)})
         )
-    def info_slack():
-      slack_info(
-      message="Click [here](https://www.vg.no)  to visit our website."
-      )
+    }
+)
+def info_slack():
+    CustomSlackOperator(
+    task_id='send_slack_message',
+    text="Click [here](https://vg.no) to visit our website.",
+    dag=dag
+)
 
-    info_slack()
+info_slack()
   
