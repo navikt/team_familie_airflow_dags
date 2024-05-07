d5987d6400491cf6753bbbba3e7c86e53934660b
Henriksen <gard.sigurd.troim.henriksen@nav.no>
2024-05-07 09:15:01 +0200

    Fjernet forsøk på emojie, siden det ikke fungerte. Kan foresøke igjen en annen gang i test_r

--- dags/kvalitet_rapport.py
+++ dags/kvalitet_rapport.py
@@ -213,7 +213,7 @@ with DAG(
 """
     # Når det oppdages et hull, vil det komme en notification for alle i channel. Dette bare konkatineres til slutten av summary string
     if any(s != [] for s in [bt_hull,ef_hull,ks_hull,pp_hull,fp_hull]): 
-           konsumenter_summary = konsumenter_summary + f"```<!channel> NB, hull i et kosument! :fire:```"
+           konsumenter_summary = konsumenter_summary + f"```<!channel> NB, hull i et kosument!```"
 
     kafka_summary = f"*Kafka rapport:*\n{konsumenter_summary}"
 
