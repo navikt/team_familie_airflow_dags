bee0297171287e2a24fb705c57b9a6408c612677
Henriksen <gard.sigurd.troim.henriksen@nav.no>
2024-05-08 10:25:36 +0200

    Prøver å rette opp i feil i formatering

--- dags/kvalitet_rapport.py
+++ dags/kvalitet_rapport.py
@@ -216,14 +216,14 @@ with DAG(
         # Sjekker om listen er tom, betyr ikke noe hull oppdaget
         if sublist:
             # Henter navn på topic
-            topics_med_hull += str(sublist[1]) + ","
+            topics_med_hull += str(sublist[1]) + ", "
 
     # Sjekker om noe ble lagt til i string, betyr hull oppdaget
     if topics_med_hull:
         # Fjerner siste komma
-        topics_med_hull = topics_med_hull[:-1]
+        topics_med_hull = topics_med_hull[:-2]
         # Konkatinerer en notification med navn på topics med hull til summary
-        konsumenter_summary = konsumenter_summary + f"```<!channel> Minst ett hull oppdaget i {topics_med_hull}!```"
+        konsumenter_summary = konsumenter_summary + f"*\n```<!channel> Minst ett hull oppdaget i {topics_med_hull}!```"
 
     kafka_summary = f"*Kafka rapport:*\n{konsumenter_summary}"
 
