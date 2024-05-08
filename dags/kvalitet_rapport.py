6045af47a100fc296735d6af231ff33873e39037
Henriksen <gard.sigurd.troim.henriksen@nav.no>
2024-05-08 11:57:45 +0200

    Endret fra +=

--- dags/kvalitet_rapport.py
+++ dags/kvalitet_rapport.py
@@ -217,14 +217,14 @@ with DAG(
         # Sjekker om listen er tom, betyr ikke noe hull oppdaget
         if sublist:
             # Henter navn på topic
-            topics_med_hull += str(sublist[1]) + f", "
+            topics_med_hull = topics_med_hull + str(sublist[1]) + f", "
 
     # Sjekker om noe ble lagt til i string, betyr hull oppdaget
     if topics_med_hull:
         # Fjerner siste komma og mellomrom
         topics_med_hull = topics_med_hull[:-2]
         # Konkatinerer en notification med navn på topics med hull til summary
-        konsumenter_summary += f"<!channel> Minst ett hull oppdaget i TEST!"
+        konsumenter_summary = konsumenter_summary + f"```<!channel> Minst ett hull oppdaget i {topics_med_hull}!```"
 
     kafka_summary = f"*Kafka rapport:*\n{konsumenter_summary}"
 
--- test.py
+++ test.py
@@ -1,17 +1,6 @@
-import datetime
+s = f""
 
+for i in ["bt_hull","ef_hull"]:
+    s += i+ f", "
 
-def get_periode():
-    """
-    henter periode for the tidligere måneden eksample--> i dag er 19.04.2022, metoden vil kalkulerer periode aarMaaned eks) '202203'
-    :param periode:
-    :return: periode
-    """
-    today = datetime.date.today() # dato for idag 2022-04-19
-    first = today.replace(day=1) # dato for første dag i måneden 2022-04-01
-    lastMonth = first - datetime.timedelta(days=1) # dato for siste dag i tidligere måneden
-
-    return lastMonth.strftime("%Y%m") # henter bare aar og maaned
-
-periode = get_periode()
-print(periode)
+print(s)
