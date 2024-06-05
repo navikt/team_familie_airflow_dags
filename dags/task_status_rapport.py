2c1bf20688573549a86e34ab203caaa212aab178
Henriksen <gard.sigurd.troim.henriksen@nav.no>
2024-06-05 13:41:31 +0200

    Kommenterte ut feil Airflow oppdaget

--- dags/task_status_rapport.py
+++ dags/task_status_rapport.py
@@ -27,7 +27,7 @@ else:
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime.datetime(2024, 5, 30),
+    #'start_date': datetime.datetime(2024, 5, 30),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
