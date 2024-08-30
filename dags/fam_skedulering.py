517efe5f254ffe192d0e9e2f6a0d087317ff5a53
Helen Rong <helen.rong@nav.no>
2024-08-30 12:22:59 +0200

    Lagt inn requirement når kaller python operator i fam skedulering

--- dags/fam_skedulering.py
+++ dags/fam_skedulering.py
@@ -57,6 +57,7 @@ with DAG(
         resources=client.V1ResourceRequirements(
             requests={"memory": "4G"},
             limits={"memory": "4G"}),
+        requirements_path="requirements.txt",
         slack_channel=Variable.get("slack_error_channel")
     )
 
