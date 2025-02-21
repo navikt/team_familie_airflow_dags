1038de503832fc239e324e3c5fcd78f2b9d73923
Helen Rong <helen.rong@nav.no>
2025-02-21 14:10:02 +0100

    SSB

--- Oracle_python/ssb_fp.py
+++ Oracle_python/ssb_fp.py
@@ -2,22 +2,17 @@ from felles_metoder.felles_metoder import oracle_secrets
 import oracledb, os, json
 
 def hent_data_fra_oracle():
-  print('Test1')
   oracle_info = oracle_secrets()
-  print('Test2')
 
   user = oracle_info['user'] + '[DVH_FAM_FP]'
-  print('Test3')
   print(user) #Test
   dsn_tns = oracledb.makedsn(oracle_info['host'], 1521, service_name = oracle_info['service'])
-  print('Test4')
 
   with oracledb.connect(user=user, password = oracle_info['password'], dsn=dsn_tns) as conn:
       with conn.cursor() as cursor:
-          print('Test5')
-          #sql_query = f"SELECT * FROM {oracle_view} where arstall = {year} and kvartal = {quarter}"
-          #cursor.execute(sql_query)
-          print(user) #Test
+          sql_query = f"select count(1) from vfam_fp_sp_ssb_2024_m"
+          cursor.execute(sql_query)
+          print(sql_query)
           conn.commit()
 
 if __name__ == "__main__":
