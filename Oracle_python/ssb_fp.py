2dfb5246f27050fa789f47dbacf0876f5cfaf5bf
Helen Rong <helen.rong@nav.no>
2025-02-21 13:45:22 +0100

    SSB

--- Oracle_python/ssb_fp.py
+++ Oracle_python/ssb_fp.py
@@ -2,18 +2,23 @@ from felles_metoder.felles_metoder import oracle_secrets
 import oracledb, os, json
 
 def hent_data_fra_oracle_fp():
-  oracle_secrets = oracle_secrets()
+  print('Test1')
+  oracle_info = oracle_secrets()
+  print('Test2')
 
-  user = oracle_secrets['user'] + '[DVH_FAM_FP]'
+  user = oracle_info['user'] + '[DVH_FAM_FP]'
+  print('Test3')
   print(user) #Test
-  dsn_tns = oracledb.makedsn(oracle_secrets['host'], 1521, service_name = oracle_secrets['service'])
+  dsn_tns = oracledb.makedsn(oracle_info['host'], 1521, service_name = oracle_info['service'])
+  print('Test4')
 
-  with oracledb.connect(user=user, password = oracle_secrets['password'], dsn=dsn_tns) as conn:
+  with oracledb.connect(user=user, password = oracle_info['password'], dsn=dsn_tns) as conn:
       with conn.cursor() as cursor:
+          print('Test5')
           #sql_query = f"SELECT * FROM {oracle_view} where arstall = {year} and kvartal = {quarter}"
           #cursor.execute(sql_query)
           print(user) #Test
           conn.commit()
 
 if __name__ == "__main__":
-   hent_data_fra_oracle_fp()
+   hent_data_fra_oracle_fp
