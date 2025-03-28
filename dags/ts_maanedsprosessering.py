2ac9b05823320e0d4754ee102c0bb7e272bc4b3a
Helen Rong <helen.rong@nav.no>
2025-03-28 13:57:39 +0100

    la til mer info i notification

--- dags/ts_maanedsprosessering.py
+++ dags/ts_maanedsprosessering.py
@@ -58,7 +58,7 @@ with DAG(
     )
     def notification_start():
         slack_info(
-            message = "Starter månedsprosessering av tilleggsstønader! :rocket:"
+            message = "Starter månedsprosessering {periode} med gyldig_flagg={gyldig_flagg} av tilleggsstønader! :rocket:"
         )
 
     start_alert = notification_start()
@@ -83,7 +83,7 @@ with DAG(
     )
     def notification_end():
         slack_info(
-            message = "Måndedsprosessering av tilleggsstønader fullført! :tada: :tada:" #TODO
+            message = "Måndedsprosessering {periode} med gyldig_flagg={gyldig_flagg} av tilleggsstønader fullført! :tada: :tada:" #TODO
         )
 
     slutt_alert = notification_end()
