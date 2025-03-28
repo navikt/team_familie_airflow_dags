4f37d0a8f55760bad026ad057e6f2bc1dc62c9b3
AArafaR <97885051+AArafaR@users.noreply.github.com>
2025-03-28 10:42:59 +0100

    opprettet en dag som leser inn barnbidrag historisk data fra sftp til oracle

---
+++ dags/bb_historisk_data.py
@@ -0,0 +1,75 @@
+from airflow.models import DAG, Variable
+from airflow.utils.dates import datetime
+from dataverk_airflow import notebook_operator
+from airflow.decorators import task
+from kubernetes import client
+from operators.slack_operator import slack_info
+from allowlists.allowlist import slack_allowlist, prod_oracle_conn_id, dev_oracle_conn_id,r_oracle_conn_id
+
+miljo = Variable.get('miljo')
+branch = Variable.get("branch")
+allowlist = []
+
+if miljo == 'Prod':
+    allowlist.extend(prod_oracle_conn_id)
+elif miljo == 'test_r':
+    allowlist.extend(r_oracle_conn_id)
+else:
+    allowlist.extend(dev_oracle_conn_id)
+
+with DAG(
+  dag_id = 'BB_historisk_data_sftp_oracle',
+  description = 'Leser barnbidrag fil fra sfto server, transformerer data og så gjøre insert til en tabell i Oracle database',
+  start_date=datetime(2025, 3, 28),
+  schedule_interval= '0 0 2 * *',
+  max_active_runs=1,
+  catchup = False
+) as dag:
+
+    @task(
+        executor_config={
+            "pod_override": client.V1Pod(
+                metadata=client.V1ObjectMeta(annotations={"allowlist":  ",".join(slack_allowlist)})
+            )
+        }
+    )
+    def notification_start():
+        slack_info(
+            message = f'Lesing av barnbidrag data fra SFTP serveren til Oracle i {miljo} database starter nå! :rocket:'
+        )
+
+    start_alert = notification_start()
+
+    bb_historisk_data = notebook_operator(
+    dag = dag,
+    name = 'BB_historisk_data_kopiering',
+    repo = 'navikt/dvh-fam-notebooks',
+    nb_path = 'BB/bb_bis_en_periode.ipynb',
+    allowlist=allowlist,
+    branch = branch,
+    #delete_on_finish= False,
+    resources=client.V1ResourceRequirements(
+        requests={'memory': '4G'},
+        limits={'memory': '4G'}),
+    slack_channel = Variable.get('slack_error_channel'),
+    requirements_path="requirements.txt",
+    #image='ghcr.io/navikt/dvh_familie_image:2023-11-27-eccc5e8-main',
+    log_output=False
+    )
+
+    @task(
+        executor_config={
+            "pod_override": client.V1Pod(
+                metadata=client.V1ObjectMeta(annotations={"allowlist": ",".join(slack_allowlist)})
+            )
+        }
+    )
+    def notification_end():
+        slack_info(
+            message = f'Historisk barnbidrag data fra SFTP til Oracle i {miljo} database er vellykket! :tada: :tada:'
+        )
+    slutt_alert = notification_end()
+
+start_alert >> bb_historisk_data >> slutt_alert
+
+
