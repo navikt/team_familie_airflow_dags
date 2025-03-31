736123998436ebb2e0282ea391459aae06757038
Henriksen <gard.sigurd.troim.henriksen@nav.no>
2025-03-31 09:30:13 +0200

    Laget en annen version av ts_maanedsprosessering som henter parametere direkte i dag

---
+++ dags/ts_maanedsprosessering_v2.py
@@ -0,0 +1,99 @@
+from airflow.models import DAG, Variable
+from airflow.utils.dates import datetime, timedelta
+from operators.dbt_operator import create_dbt_operator
+from operators.slack_operator import slack_info, slack_error
+from airflow.decorators import task
+from kubernetes import client
+from felles_metoder.felles_metoder import get_periode
+from allowlists.allowlist import slack_allowlist, prod_oracle_conn_id, dev_oracle_conn_id, r_oracle_conn_id
+
+miljo = Variable.get('miljo')   
+allowlist = []
+if miljo == 'Prod':
+    allowlist.extend(prod_oracle_conn_id)
+elif miljo == 'test_r':
+    allowlist.extend(r_oracle_conn_id)
+else:
+    allowlist.extend(dev_oracle_conn_id)
+
+default_args = {
+    'owner': 'Team-Familie', 
+    'retries': 2, 
+    'retry_delay': timedelta(minutes=1),
+    'on_failure_callback': slack_error
+    }
+
+# Bygger parameter med logging, modeller og miljø
+settings = Variable.get("dbt_ef_schema", deserialize_json=True)
+v_branch = settings["branch"]
+v_schema = settings["schema"]
+#v_periode = settings["periode"]
+#v_gyldig_flagg = settings["gyldig_flagg"]
+
+# Definerer DAG-nivå standardverdier for periode og gyldig_flagg
+dag_params = {
+    "periode": None,  # Standardverdi er None, vil bli satt dynamisk hvis tom
+    "gyldig_flagg": None  # Standardverdi er None, vil bli satt dynamisk hvis tom
+}
+
+with DAG(
+    dag_id='ts_maanedsprosessering_v2',
+    description='DAG som kjører insert i fag_ts_mottaker basert på periode',
+    default_args=default_args,
+    start_date=datetime(2024, 10, 4),
+    schedule_interval='0 0 5 * *',  # Den 5. hver måned
+    catchup=True,
+    params=dag_params  # Legger til DAG-nivå parametere
+) as dag:
+
+    # Setter periode og gyldig_flagg basert på dag.params eller fallback-logikk
+    if dag.params.get("periode") is None or dag.params.get("periode") == '':
+        periode = get_periode()
+    else:
+        periode = dag.params.get("periode")
+
+    if dag.params.get("gyldig_flagg") is None or dag.params.get("gyldig_flagg") == '':
+        gyldig_flagg = 1  # Standardverdi
+    else:
+        gyldig_flagg = dag.params.get("gyldig_flagg")
+
+    @task(
+        executor_config={
+            "pod_override": client.V1Pod(
+                metadata=client.V1ObjectMeta(annotations={"allowlist": ",".join(slack_allowlist)})
+            )
+        }
+    )
+    def notification_start():
+        slack_info(
+            message=f"Starter månedsprosessering {periode} med gyldig_flagg={gyldig_flagg} av tilleggsstønader! :rocket:"
+        )
+
+    start_alert = notification_start()
+
+    ts_dbt_insert = create_dbt_operator(
+        dag=dag,
+        name="insert_mottaker_data",
+        repo='navikt/dvh_fam_ts_dbt',
+        script_path='airflow/dbt_run.py',
+        branch=v_branch,
+        dbt_command=f"""run --select TS_maanedsprosessering_v2.* --vars '{{"periode":"{periode}", "gyldig_flagg":{gyldig_flagg}}}' """,
+        allowlist=allowlist,
+        db_schema=v_schema
+    )
+
+    @task(
+        executor_config={
+            "pod_override": client.V1Pod(
+                metadata=client.V1ObjectMeta(annotations={"allowlist": ",".join(slack_allowlist)})
+            )
+        }
+    )
+    def notification_end():
+        slack_info(
+            message=f"Måndedsprosessering {periode} med gyldig_flagg={gyldig_flagg} av tilleggsstønader fullført! :tada: :tada:"
+        )
+
+    slutt_alert = notification_end()
+
+start_alert >> ts_dbt_insert >> slutt_alert
