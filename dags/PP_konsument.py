9a8851a27c5273fe33ba85cc1e26c67e2e98381b
AArafaR <97885051+AArafaR@users.noreply.github.com>
2025-05-06 14:50:36 +0200

    endret repo til pp_dbt

--- dags/PP_konsument.py
+++ dags/PP_konsument.py
@@ -47,7 +47,7 @@ with DAG(
   pp_utpakking_dbt = create_dbt_operator(
       dag=dag,
       name="utpakking_pp",
-      repo='navikt/dvh_familie_dbt',
+      repo='navikt/dvh_fam_pp_dbt',
       script_path = 'airflow/dbt_run.py',
       branch=v_branch,
       dbt_command= "run --select PP_utpakking.*",
