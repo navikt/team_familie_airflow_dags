44cb9e0503791c5f031c0a614c96deb84b42b892
AArafaR <97885051+AArafaR@users.noreply.github.com>
2025-05-13 15:24:40 +0200

    flyttet parse_task_image til felles_metoder

--- felles_metoder/felles_metoder.py
+++ felles_metoder/felles_metoder.py
@@ -1,6 +1,7 @@
-import datetime, os, json
+import datetime, os, json, re
 from os import getenv
 from google.cloud import secretmanager
+from pathlib import Path
 
 def get_periode():
     """
@@ -35,3 +36,28 @@ def oracle_secrets():
     encoding="UTF-8",
     nencoding="UTF-8"
     )
+
+def parse_task_image(name: str) -> str:
+    """This function parses the Dockerfile of a task image and returns the image reference.
+
+    It assumes that the Dockerfile is located in /dag/task_images/{name}/Dockerfile.
+
+    The Dockerfile must contain a single FROM statement, and the image reference is returned.
+
+    Arguments:
+        name (str): The name of the directory in dag/task_images that contains the Dockerfile.
+            May contain slashes to indicate subdirectories.
+    """
+    project_root = Path(__file__).resolve().parents[1]
+
+    dockerfile = project_root / "dags" / "task_images" / name / "Dockerfile"
+    if not dockerfile.exists():
+        raise RuntimeError(f"Could not find Dockerfile {dockerfile}")
+
+    with dockerfile.open("r", encoding="utf-8") as f:
+        matches = re.findall(r"^FROM\s+(.*)", f.read(), flags=re.MULTILINE)
+        if len(matches) == 1:
+            return matches[0].strip()
+        else:
+            rel_path = dockerfile.relative_to(project_root)
+            raise RuntimeError(f"Could not find a unique image reference in Dockerfile {rel_path}")
--- siste_image_versjon.py
+++ siste_image_versjon.py
@@ -28,30 +28,5 @@ def get_latest_ghcr_tag(repo: str) -> str:
     print(f"Siste tag for ghcr.io/navikt/{repo}:{latest_tag}")
     return latest_tag
 
-
-def parse_task_image(name: str) -> str:
-    """This function parses the Dockerfile of a task image and returns the image reference.
-
-    It assumes that the Dockerfile is located in /dag/task_images/{name}/Dockerfile.
-
-    The Dockerfile must contain a single FROM statement, and the image reference is returned.
-
-    Arguments:
-        name (str): The name of the directory in dag/task_images that contains the Dockerfile.
-            May contain slashes to indicate subdirectories.
-    """
-    project_root = Path(__file__).resolve().parents[1]
-
-    dockerfile = project_root / "dags" / "task_images" / name / "Dockerfile"
-    if not dockerfile.exists():
-        raise RuntimeError(f"Could not find Dockerfile {dockerfile}")
-
-    with dockerfile.open("r", encoding="utf-8") as f:
-        matches = re.findall(r"^FROM\s+(.*)", f.read(), flags=re.MULTILINE)
-        if len(matches) == 1:
-            return matches[0].strip()
-        else:
-            rel_path = dockerfile.relative_to(project_root)
-            raise RuntimeError(f"Could not find a unique image reference in Dockerfile {rel_path}")
     
 
