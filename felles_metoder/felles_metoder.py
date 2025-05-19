d4b1313607b7b22eb5f44e809665c71a4d4a3a96
AArafaR <97885051+AArafaR@users.noreply.github.com>
2025-05-19 14:19:22 +0200

    oppdatert konsumentene våre til å med den nyeste versjonen av dvh-airflow-kafka bibliotek

--- felles_metoder/felles_metoder.py
+++ felles_metoder/felles_metoder.py
@@ -1,10 +1,7 @@
-import datetime, os, json
+import datetime, os, json, re
 from os import getenv
 from google.cloud import secretmanager
-import calendar
-import datetime
-import calendar
-from dateutil.relativedelta import relativedelta
+from pathlib import Path
 
 def get_periode():
     """
@@ -17,16 +14,6 @@ def get_periode():
     lastMonth = first - datetime.timedelta(days=1) # dato for siste dag i tidligere måneden
 
     return lastMonth.strftime("%Y%m") # henter bare aar og maaned
-
-
-def get_siste_dag_i_forrige_maaned():
-    today = datetime.datetime.now()
-    first_day_of_this_month = today.replace(day=1)
-    last_day_of_last_month = first_day_of_this_month - datetime.timedelta(days=1)
-    last_day = last_day_of_last_month.day
-    return last_day_of_last_month.replace(day=last_day, hour=0, minute=0, second=0).strftime('%Y%m%d')
-
-
 
 
 def set_secrets_as_envs():
@@ -45,11 +32,32 @@ def oracle_secrets():
     password=getenv("DBT_ORCL_PASS"),
     host = getenv("DBT_ORCL_HOST"),
     service = getenv("DBT_ORCL_SERVICE"),
+    ghcr_token = getenv("ghcr_token"),
     encoding="UTF-8",
     nencoding="UTF-8"
     )
 
+def parse_task_image(name: str) -> str:
+    """This function parses the Dockerfile of a task image and returns the image reference.
 
+    It assumes that the Dockerfile is located in /dag/task_images/{name}/Dockerfile.
 
+    The Dockerfile must contain a single FROM statement, and the image reference is returned.
 
+    Arguments:
+        name (str): The name of the directory in dag/task_images that contains the Dockerfile.
+            May contain slashes to indicate subdirectories.
+    """
+    project_root = Path(__file__).resolve().parents[1]
 
+    dockerfile = project_root / "dags" / "task_images" / name / "Dockerfile"
+    if not dockerfile.exists():
+        raise RuntimeError(f"Could not find Dockerfile {dockerfile}")
+
+    with dockerfile.open("r", encoding="utf-8") as f:
+        matches = re.findall(r"^FROM\s+(.*)", f.read(), flags=re.MULTILINE)
+        if len(matches) == 1:
+            return matches[0].strip()
+        else:
+            rel_path = dockerfile.relative_to(project_root)
+            raise RuntimeError(f"Could not find a unique image reference in Dockerfile {rel_path}")
