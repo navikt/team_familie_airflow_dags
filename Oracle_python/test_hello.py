5377970b93cb306d421ff2b5c851a3f9bfb7b6c8
Helen Rong <helen.rong@nav.no>
2025-06-18 15:57:46 +0200

    Test headers

--- Oracle_python/test_hello.py
+++ Oracle_python/test_hello.py
@@ -9,6 +9,8 @@ consumer = KafkaConsumer(
         group_id='dvh_familie_konsument',
         # Add other necessary configurations (e.g., security, deserializers)
     )
+consumer = KafkaConsumer(bootstrap_servers='nav-dev-kafka-nav-dev.aivencloud.com:26484', auto_offset_reset='earliest')
+consumer.subscribe(['bidrag.statistikk-bidrag-q2'])
 
 for message in consumer:
     headers = message.bb
