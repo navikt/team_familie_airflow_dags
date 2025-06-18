ef618e779c3b1f74747be237244782bc6095ba05
Helen Rong <helen.rong@nav.no>
2025-06-18 15:45:35 +0200

    Test headers

--- Oracle_python/test_hello.py
+++ Oracle_python/test_hello.py
@@ -1,2 +1,16 @@
 print("Hello World")
 print("Hello World")
+
+from kafka import KafkaConsumer
+
+consumer = KafkaConsumer(
+        topic,
+        bootstrap_servers=['nav-dev-kafka-139'],
+        group_id='dvh_familie_konsument',
+        # Add other necessary configurations (e.g., security, deserializers)
+    )
+
+for message in consumer:
+    headers = message.bb
+    for header in headers:
+        print(f"Header Key: {header.key}, Header Value: {header.value}")
