d7584dbad64050bf56bb20799ef39751388b186d
Helen Rong <helen.rong@nav.no>
2025-06-18 16:03:08 +0200

    Test headers

--- Oracle_python/test_hello.py
+++ Oracle_python/test_hello.py
@@ -9,7 +9,7 @@ from kafka import KafkaConsumer
 #        group_id='dvh_familie_konsument',
 #        # Add other necessary configurations (e.g., security, deserializers)
 #    )
-consumer = KafkaConsumer(bootstrap_servers='nav-dev-kafka-nav-dev.aivencloud.com:26484', auto_offset_reset='earliest')
+consumer = KafkaConsumer(bootstrap_servers='nav-dev-kafka-nav-dev.aivencloud.com:26484', auto_offset_reset='earliest', group_id='dvh_familie_konsument')
 consumer.subscribe(['bidrag.statistikk-bidrag-q2'])
 
 for message in consumer:
