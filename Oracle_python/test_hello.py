b72e59c1012ca15a7b76071ffb2720eb7109b0e4
Helen Rong <helen.rong@nav.no>
2025-06-18 15:59:35 +0200

    Test headers

--- Oracle_python/test_hello.py
+++ Oracle_python/test_hello.py
@@ -3,12 +3,12 @@ print("Hello World")
 
 from kafka import KafkaConsumer
 
-consumer = KafkaConsumer(
-        topic='bidrag.statistikk-bidrag-q2',
-        bootstrap_servers=['nav-dev-kafka-nav-dev.aivencloud.com:26484'],
-        group_id='dvh_familie_konsument',
-        # Add other necessary configurations (e.g., security, deserializers)
-    )
+#consumer = KafkaConsumer(
+#        topic='bidrag.statistikk-bidrag-q2',
+#        bootstrap_servers=['nav-dev-kafka-nav-dev.aivencloud.com:26484'],
+#        group_id='dvh_familie_konsument',
+#        # Add other necessary configurations (e.g., security, deserializers)
+#    )
 consumer = KafkaConsumer(bootstrap_servers='nav-dev-kafka-nav-dev.aivencloud.com:26484', auto_offset_reset='earliest')
 consumer.subscribe(['bidrag.statistikk-bidrag-q2'])
 
