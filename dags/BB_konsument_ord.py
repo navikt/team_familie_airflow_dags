2ebc3b8cf2e67b5ba0910f7390add7c6141d4b9a
AArafaR <97885051+AArafaR@users.noreply.github.com>
2025-06-19 15:40:50 +0200

    endret konfig fila i dagen

--- dags/BB_konsument_ord.py
+++ dags/BB_konsument_ord.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from airflow.models import DAG
 from airflow.models import Variable
-from kosument_config import bb
+from kosument_config import bb_statistikk
 from operators.kafka_operators import kafka_consumer_kubernetes_pod_operator
 from operators.dbt_operator import create_dbt_operator
 from operators.slack_operator import slack_error
@@ -42,7 +42,7 @@ with DAG(
 
   consumer = kafka_consumer_kubernetes_pod_operator(
     task_id = "BB_ordinaer_hent_kafka_data",
-    config = bb.config.format(topic),
+    config = bb_statistikk.config.format(topic),
     kafka_consumer_image=parse_task_image("dvh-airflow-kafka"),
     #data_interval_start_timestamp_milli="1738281600000", # gir oss alle data som ligger på topicen fra og til (intial last alt på en gang)
     #data_interval_end_timestamp_milli="1738587540000",   # from first day we got data until 29.05.2023 (todays before todays date)
