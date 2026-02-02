075330abb095e04c03ed24eea3042191a77d01f7
AArafaR <97885051+AArafaR@users.noreply.github.com>
2026-02-02 11:20:43 +0100

    endret start date til 01.02.2026

--- dags/FP_konsument.py
+++ dags/FP_konsument.py
@@ -34,7 +34,7 @@ topic = Variable.get("FP_topic") # topic navn hentes foreløpig fra airflow vari
 
 with DAG(
   dag_id="FP_konsument",
-  start_date=datetime(2024, 4, 18, 12),
+  start_date=datetime(2026, 2, 1),
   default_args = default_args,
   schedule_interval= "@hourly",
   max_active_runs=1,
