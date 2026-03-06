aea42d72518e8b365cbfbbbb2287fd58981df9a4
AArafaR <97885051+AArafaR@users.noreply.github.com>
2026-03-06 11:59:47 +0100

    pakket fam_skedulering jobben i try except, skriver ut feilmeldingen i loggen

--- Oracle_python/fam_skedulering.py
+++ Oracle_python/fam_skedulering.py
@@ -1,26 +1,44 @@
+import logging
 import oracledb
 from felles_metoder.felles_metoder import oracle_secrets
 
+logger = logging.getLogger(__name__)
+
 def fam_skedulering():
 
-    send_context_sql = (f'''
+    send_context_sql = '''
         begin
             dbms_application_info.set_client_info( client_info => 'Klient_info Familie-Airflow');
             dbms_application_info.set_module( module_name => 'Kjører Team-familie Airflow applikasjon'
                                             , action_name => 'Skedulere familie pakke som ligger i oracle' );
         end;
-    ''')
-    
+    '''
+
     secrets = oracle_secrets()
 
-    dsn_tns = oracledb.makedsn(secrets['host'], 1521, service_name = secrets['service'])
-    with oracledb.connect(user = secrets['user']+'[dvh_fam_fp]', password = secrets['password'], dsn = dsn_tns) as connection:
-        with connection.cursor() as cursor:
-            cursor.execute(send_context_sql)     
-                 
-            cursor.callproc('fam_skedulering.fam_skedulering', [None, None])
+    try:
+        dsn_tns = oracledb.makedsn(secrets['host'], 1521, service_name=secrets['service'])
 
-            connection.commit()
+        with oracledb.connect(user=secrets['user']+'[dvh_fam_fp]', password=secrets['password'], dsn=dsn_tns) as connection:
+            with connection.cursor() as cursor:
+
+                logger.info("Sending Oracle context")
+
+                cursor.execute(send_context_sql)
+
+                logger.info("Calling procedure fam_skedulering.fam_skedulering")
+
+                cursor.callproc('fam_skedulering.fam_skedulering', [None, None])
+
+                connection.commit()
+
+                logger.info("Procedure completed successfully")
+
+    except oracledb.DatabaseError as e:
+        error, = e.args
+        logger.error("Oracle error code: %s", error.code)
+        logger.error("Oracle error message: %s", error.message)
+        raise
 
 if __name__ == "__main__":
     fam_skedulering()
