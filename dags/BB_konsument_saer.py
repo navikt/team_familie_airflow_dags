60562bac1a8d2809231f804113bc65ec573b691f
nav-martenordin <marte.nordin@nav.no>
2026-08-05 12:09:16 +0200

    BB Særbidrag Dag, la til utpakking i BB_konsument_saer. (#117)

--- dags/BB_konsument_saer.py
+++ dags/BB_konsument_saer.py
@@ -44,4 +44,15 @@ with DAG(
   )
 
 
-consumer
+  bb_saer_utpakking_dbt = create_dbt_operator(
+     dag=dag,
+     name="utpakking_bb_saer",
+     repo='navikt/dvh_fam_bb',
+     script_path = 'airflow/dbt_run.py',
+     branch=v_branch,
+     dbt_command= """run --select BB_utpakking_saerbidrag.*""",
+     db_schema=v_schema,
+     allowlist=allowlist
+ )
+
+consumer >> bb_saer_utpakking_dbt
